c9fae358673592ea55c56784928b4cd68aee6fe4
Rishabhcodes65536 <jain.rishabh32768@gmail.com>
2024-04-24 06:12:04 +0530

    minor change

--- backend/models/music.js
+++ backend/models/music.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 import validator from "mongoose-validator"
 
 const musicSchema=new mongoose.Schema({
-    musicname:{type:String , trim:true , required:true},
+    music:{type:String , required:true},
     description:{type:String},
     link:{type:String},
 })
 
 const musicModel=mongoose.model('music',musicSchema)
 
-export default musicModel
+export default musicModel;
